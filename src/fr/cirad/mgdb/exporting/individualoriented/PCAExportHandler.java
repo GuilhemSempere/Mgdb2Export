3c69f8816a03631806299b4e629532b9d2c0febe
Guilhem Sempéré <guilhem.sempere@cirad.fr>
2026-06-10 12:31:28 +0200

    Fix in supporting unplaced variants at export time

--- src/fr/cirad/mgdb/exporting/individualoriented/PCAExportHandler.java
+++ src/fr/cirad/mgdb/exporting/individualoriented/PCAExportHandler.java
@@ -271,8 +271,8 @@ public class PCAExportHandler extends EigenstratExportHandler {
 		        while (markerCursor.hasNext()) {
 		            Document exportVariant = markerCursor.next();
 		            Document refPos = (Document) Helper.readPossiblyNestedField(exportVariant, refPosPath, ";", null);
-		            Long pos = refPos == null ? null : ((Number) refPos.get(ReferencePosition.FIELDNAME_START_SITE)).longValue();
-		            String chrom = refPos == null ? null : (String) refPos.get(ReferencePosition.FIELDNAME_SEQUENCE);
+		            Long pos = refPos == null || refPos.isEmpty() ? null : ((Number) refPos.get(ReferencePosition.FIELDNAME_START_SITE)).longValue();
+		            String chrom = refPos == null || refPos.isEmpty() ? null : (String) refPos.get(ReferencePosition.FIELDNAME_SEQUENCE);
 	                String markerId = (String) exportVariant.get("_id");
 		            if (chrom == null)
 		            	unassignedMarkers.add(markerId);
